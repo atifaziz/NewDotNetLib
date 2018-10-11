c47d865286384c4d006b504b9ad5d433acea78e2
Atif Aziz <code@raboof.com>
2018-10-11 10:12:39 +0200

    Windows batch to create new project from this template

---
+++ go.cmd
@@ -0,0 +1,34 @@
+@echo off
+setlocal
+set PROJECT_NAME=%~nx1
+set THIS_DIR=%~dp0
+IF %THIS_DIR:~-1%==\ SET THIS_DIR=%THIS_DIR:~0,-1%
+git clone "%THIS_DIR%" %1 || goto :EOF
+pushd %1
+    rd /s /q .git                                               ^
+ && git init                                                    ^
+ && git config user.name "Atif Aziz"                            ^
+ && git config user.email "atifaziz@users.noreply.github.com"   ^
+ && dotnet new sln                                              ^
+ && dotnet new classlib -o src -n "%PROJECT_NAME%"              ^
+ && dotnet new nunit -o tests -n "%PROJECT_NAME%.Tests"         ^
+ && dotnet sln add src                                          ^
+ && dotnet sln add tests                                        ^
+ && dotnet add tests reference src                              ^
+ && dotnet test tests                                           ^
+ && git add .                                                   ^
+ && git add --chmod +x *.sh
+popd
+endlocal
+if not %ERRORLEVEL%==0 goto :EOF
+echo ................................................
+echo .########..########....###....########..##....##
+echo .##.....##.##.........##.##...##.....##..##..##.
+echo .##.....##.##........##...##..##.....##...####..
+echo .########..######...##.....##.##.....##....##...
+echo .##...##...##.......#########.##.....##....##...
+echo .##....##..##.......##.....##.##.....##....##...
+echo .##.....##.########.##.....##.########.....##...
+echo ................................................
+echo May the Force be with you!
+cd %1
