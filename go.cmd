64649b98764cda774d2551e51f517be93aeb337a
Atif Aziz <code@raboof.com>
2018-10-11 10:31:48 +0200

    Add arg validation to go.cmd

--- go.cmd
+++ go.cmd
@@ -5,6 +5,10 @@ if "%1"=="-v" (
     shift
     echo on
 )
+if "%1"=="" (
+    echo>&2 Missing new project path specification!
+    exit /b 1
+)
 set PROJECT_NAME=%~nx1
 IF %THIS_DIR:~-1%==\ SET THIS_DIR=%THIS_DIR:~0,-1%
 git clone "%THIS_DIR%" %1 || goto :EOF
