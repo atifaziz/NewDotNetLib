7d038115e7fc3a08b0f4a18ee98ee077124a7f96
Atif Aziz <code@raboof.com>
2018-12-13 13:08:12 +0100

    Swap dotnet --info with restoring

--- build.cmd
+++ build.cmd
@@ -5,8 +5,8 @@ popd
 goto :EOF
 
 :main
-    dotnet restore ^
- && dotnet --info ^
+    dotnet --info ^
+ && dotnet restore ^
  && dotnet build --no-restore -c Debug ^
  && dotnet build --no-restore -c Release
 goto :EOF
